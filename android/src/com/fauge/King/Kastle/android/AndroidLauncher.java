21b5989ecdb59ce14aa809e4ab7e472b840cc96d
fauge7 <biwydakid50@gmail.com>
2014-09-10 14:46:17 -0700

    Initial Commit
    Initial

---
+++ android/src/com/fauge/King/Kastle/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fauge.King.Kastle.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fauge.King.Kastle.KingKastleMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KingKastleMain(), config);
+	}
+}
---
+++ desktop/src/com/fauge/King/Kastle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fauge.King.Kastle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fauge.King.Kastle.KingKastleMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KingKastleMain(), config);
+	}
+}
